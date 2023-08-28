11c510e76258fd90d91911236cbf46a175fbe668
Filip Tibell <filip.tibell@gmail.com>
2023-08-28 18:20:24 -0500

    Add helper methods to token kind

--- src/toml/token/kind.rs
+++ src/toml/token/kind.rs
@@ -15,6 +15,44 @@ pub enum TokenKind {
     Integer,
 }
 
+impl TokenKind {
+    pub fn is_dot(&self) -> bool {
+        *self == Self::Dot
+    }
+
+    pub fn is_equals(&self) -> bool {
+        *self == Self::Equals
+    }
+
+    pub fn is_left_bracket(&self) -> bool {
+        *self == Self::LeftBracket
+    }
+
+    pub fn is_left_brace(&self) -> bool {
+        *self == Self::LeftBrace
+    }
+
+    pub fn is_right_bracket(&self) -> bool {
+        *self == Self::RightBracket
+    }
+
+    pub fn is_right_brace(&self) -> bool {
+        *self == Self::RightBrace
+    }
+
+    pub fn is_comment(&self) -> bool {
+        *self == Self::Comment
+    }
+
+    pub fn is_string(&self) -> bool {
+        *self == Self::String
+    }
+
+    pub fn is_integer(&self) -> bool {
+        *self == Self::Integer
+    }
+}
+
 impl From<RawToken<'_>> for TokenKind {
     fn from(value: RawToken) -> Self {
         match value {
