6cb2bc56e8882d2462ba6c049b9f01af5ebdbac2
Filip Tibell <filip.tibell@gmail.com>
2023-09-02 09:32:21 -0500

    Temp parsing improvements for manifest dependencies

--- src/tools/cargo/manifest.rs
+++ src/tools/cargo/manifest.rs
@@ -4,6 +4,8 @@ use serde::Deserialize;
 use toml::{Spanned, Value as TomlValue};
 use tracing::error;
 
+const INVALID_DEPS_PANIC_MSG: &str = "Invalid deps should have been filtered out";
+
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 #[serde(transparent)]
 pub struct ManifestDependency(Spanned<TomlValue>);
@@ -28,7 +30,14 @@ impl ManifestDependency {
 
 impl fmt::Display for ManifestDependency {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.0.as_ref().fmt(f)
+        match self.0.as_ref() {
+            TomlValue::String(s) => s.fmt(f),
+            TomlValue::Table(t) => match t.get("version").expect(INVALID_DEPS_PANIC_MSG) {
+                TomlValue::String(s) => s.fmt(f),
+                _ => unreachable!("{INVALID_DEPS_PANIC_MSG}"),
+            },
+            _ => unreachable!("{INVALID_DEPS_PANIC_MSG}"),
+        }
     }
 }
 
@@ -44,10 +53,25 @@ pub struct Manifest {
 
 impl Manifest {
     pub fn parse(source: impl AsRef<str>) -> Result<Self, toml::de::Error> {
-        let result = toml::from_str::<Manifest>(source.as_ref());
+        let result = toml::from_str::<Manifest>(source.as_ref())
+            .map(|manifest| manifest.remove_invalid_deps());
         if let Err(e) = &result {
             error!("failed to deserialize cargo manifest - {e}")
         }
         result
+    }
+
+    fn remove_invalid_deps(mut self) -> Self {
+        let check = |deps: &mut HashMap<String, ManifestDependency>| {
+            deps.retain(|_, val| match val.0.as_ref() {
+                TomlValue::String(_) => true,
+                TomlValue::Table(t) => matches!(t.get("version"), Some(TomlValue::String(_))),
+                _ => false,
+            });
+        };
+        check(&mut self.dependencies);
+        check(&mut self.dev_dependencies);
+        check(&mut self.build_dependencies);
+        self
     }
 }
