4445e63dc8f11d94ef1f8f8de1215256edcc575c
Filip Tibell <filip.tibell@gmail.com>
2023-09-17 19:22:19 -0500

    Fix join_all implementation

--- src/util/futures.rs
+++ src/util/futures.rs
@@ -3,13 +3,17 @@ use smol::Executor;
 
 pub async fn join_all<F, T>(futs: Vec<F>) -> Vec<T>
 where
-    F: Future<Output = T>,
-    T: Send + Sync + 'static,
+    F: Future<Output = T> + Send,
+    T: Send,
 {
     let exec = Executor::new();
+    let mut handles = Vec::new();
+    for fut in futs {
+        handles.push(exec.spawn(fut));
+    }
     exec.run(async move {
         let mut results = Vec::new();
-        for handle in futs {
+        for handle in handles {
             results.push(handle.await);
         }
         results
